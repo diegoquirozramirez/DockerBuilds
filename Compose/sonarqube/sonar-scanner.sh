3e00bb405301dfd1349b4cfefe84b2d601848c3b
Administrator <ingquirozramirez@gmail.com>
2021-01-30 22:43:57 -0500

    anotaciones sonar + jenkins

---
+++ Compose/sonarqube/sonar-scanner.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+apt-get update
+apt-get install unzip wget nodejs -y
+
+mkdir /downloads/sonarqube -p
+cd /downloads/sonarqube
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.2.0.1873-linux.zip
+unzip sonar-scanner-cli-4.2.0.1873-linux.zip
+mv sonar-scanner-4.2.0.1873-linux /opt/sonar-scanner
